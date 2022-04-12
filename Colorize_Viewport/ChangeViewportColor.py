d6efce8b6f7d100e47f54a2e1a333d4e5f25935e
unknown <lollier.guillaume@gmail.com>
2022-04-12 16:46:25 +0200

    Random color in viewport v1é

---
+++ Colorize_Viewport/ChangeViewportColor.py
@@ -0,0 +1,57 @@
+# Randomly change viewport color of all objects
+# By Guillaume Lollier
+# http://instagram.com/guillaumelollier
+# Updated: 12.04.2022
+
+import c4d, random
+from c4d import gui
+
+def GenerateRandomColor():
+    randX = random.random()
+    randY = random.random()
+    randZ = random.random()
+    return c4d.Vector(randX, randY, randZ)
+
+def ResetColor():
+    return c4d.Vector()
+
+def ChangeColor(obj):
+    obj[c4d.ID_BASEOBJECT_USECOLOR] = 2
+    obj[c4d.ID_BASEOBJECT_COLOR] = GenerateRandomColor()
+
+def ResetColor(obj):
+    obj[c4d.ID_BASEOBJECT_USECOLOR] = 0
+
+def IterateHierarchy(obj,ColorChoice):
+    while(obj):
+        if(ColorChoice):
+            ChangeColor(obj)
+        else:
+            ResetColor(obj)
+        print(obj.GetDown())
+        if(obj.GetDown() != None):
+            print('enfant')
+            IterateHierarchy(obj.GetDown(),ColorChoice)
+        obj = obj.GetNext()
+
+def message(id, data):
+    doc = c4d.documents.GetActiveDocument()
+    if id == c4d.MSG_DESCRIPTION_COMMAND:
+        id2 = data['id'][0].id
+        if id2 == c4d.ID_USERDATA:
+            userDataId = data['id'][1].id
+            if userDataId == 2:
+                obj = doc.GetFirstObject()
+                IterateHierarchy(obj,1)
+                c4d.EventAdd()
+            if userDataId == 3:
+                obj = doc.GetFirstObject()
+                IterateHierarchy(obj,0)
+                c4d.EventAdd()   
+
+def main():
+    pass
+
+# Execute main()
+if __name__=='__main__':
+    main()
